6326571621a1e4249e50df9e07e8257f215e10a9
Grant Hesketh <ghesketh76@gmail.com>
2020-10-29 20:17:15 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -4,9 +4,16 @@
 #  count= count + 1 
 #end
 
+#count = 10
+#while count > 0 do
+#  puts "#{count}"
+#  count = count - 1 
+#end
+#puts "Happy New Year!"
+
 count = 10
-while count > 0 do
-  puts "#{count}"
-  count = count - 1 
+while count >=1 do
+  puts count
+  count -= 1 
 end
 puts "Happy New Year!"
